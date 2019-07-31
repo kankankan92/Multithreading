68f175f34a7cb77f8c7f2025331a703f19dd8f53
Anastasia <kankankan92@mail.ru>
2019-07-31 16:28:20 +0300

    использование класса CountDownLatch

--- src/com/company/AverageSearchCallable.java
+++ src/com/company/AverageSearchCallable.java
@@ -3,25 +3,22 @@ package com.company;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
 
-public class AverageSearchThreadV2 extends Thread {
+public class AverageSearchCallable implements Callable<Double> {
 
     private List<String> fileNames;
     private int numberThread;
     private int countThreads;
-    private double average;
-    private CountDownLatch countDownLatch;
 
-    AverageSearchThreadV2(List<String> fileNames, int numberThread, int countThreads, CountDownLatch countDownLatch) {
+    AverageSearchCallable(List<String> fileNames, int numberThread, int countThreads) {
         this.fileNames = fileNames;
         this.numberThread = numberThread;
         this.countThreads = countThreads;
-        this.countDownLatch = countDownLatch;
     }
 
     @Override
-    public void run() {
+    public Double call() throws Exception {
+//        long start = System.currentTimeMillis();
         double sum = 0;
         int iterations = 0;
         for (int fileNumber = numberThread; fileNumber < fileNames.size(); fileNumber += countThreads) {
@@ -35,13 +32,10 @@ public class AverageSearchThreadV2 extends Thread {
                 iterations++;
             }
         }
-        average = sum / iterations;
-        countDownLatch.countDown();
-    }
-
-    public double getAverage() {
-        return average;
+        return sum / iterations;
+//        System.out.println("Cреднее арифметическое: " + average);
+//        long finish = System.currentTimeMillis();
+//        long timeConsumedMillis = finish - start;
+//        System.out.println("Время выполнения: " + timeConsumedMillis);
     }
 }
-
-
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -2,15 +2,16 @@ package com.company;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.FutureTask;
 
 public class Menu {
     public static void start() throws IOException, InterruptedException {
         Scanner scan = new Scanner(System.in);
         while (true) {
-            System.out.println("1.Сгенерировать n файлов по m строк \n2.Поиск среднего арифметического \n3.Поиск среднего арифметического в многопоточном режиме \n4.Поиск среднего арифметического в многопоточном режиме V2");
+            System.out.println("1.Сгенерировать n файлов по m строк \n2.Поиск среднего арифметического \n3.Поиск среднего арифметического в многопоточном режиме \n4.Поиск среднего арифметического в многопоточном режиме V2 \n5.Поиск среднего арифметического в многопоточном режиме V3");
             switch (scan.nextLine()) {
                 case "1":
                     System.out.println("Введите колличество файлов:");
@@ -23,7 +24,9 @@ public class Menu {
                 case "2":
                     MathUtils.averageInAllFiles();
                     break;
+
                 case "3":
+
                     //реализация подсчета сренего арифметического с помощью Thread и метода join().
                     long start = System.currentTimeMillis();
                     int countThreads = 10;
@@ -43,25 +46,61 @@ public class Menu {
                         }
                     }
                     double sum = 0;
-                    for (AverageSearchThread thread: averageSearchThreads) {
+                    for (AverageSearchThread thread : averageSearchThreads) {
                         thread.join();
                         sum += thread.getAverage();
                     }
                     double average = sum / averageSearchThreads.size();
-                    System.out.println("Среднее арифметическое по всем файлам:" +average);
+                    System.out.println("Среднее арифметическое по всем файлам:" + average);
                     long finish = System.currentTimeMillis();
                     long timeConsumedMillis = finish - start;
                     System.out.println("Время выполнения: " + timeConsumedMillis);
                     break;
+
                 case "4":
-                    countThreads = 10;
+
+                    //реализация подсчета сренего арифметического с помощью Thread, класса CountDownLatch и его методов await() и countDown().
+                    long start2 = System.currentTimeMillis();
                     List<String> fileNames1 = MyFileUtils.getFilesNames();
-                    for (int i = 1; i <= countThreads; i++) {
-                        AverageSearchThreadV2 averageSearchThreadV2 = new AverageSearchThreadV2(fileNames1, i, countThreads);
+                    if (fileNames1.size() > 10) {
+                        countThreads = 10;
+                    } else {
+                        countThreads = fileNames1.size();
+                    }
+
+                    CountDownLatch countDownLatch = new CountDownLatch(countThreads);
+                    List<AverageSearchThreadV2> averageSearchThreads2 = new ArrayList<>();
+                    for (int i = 0; i < countThreads; i++) {
+                        AverageSearchThreadV2 averageSearchThreadV2 = new AverageSearchThreadV2(fileNames1, i, countThreads, countDownLatch);
+                        averageSearchThreads2.add(averageSearchThreadV2);
                         averageSearchThreadV2.start();
                     }
+                    countDownLatch.await();
+                    double sum2 = 0;
+                    for (AverageSearchThreadV2 thread : averageSearchThreads2) {
+                        sum2 += thread.getAverage();
+                    }
+                    double average2 = sum2 / averageSearchThreads2.size();
+                    System.out.println("Среднее арифметическое по всем файлам:" + average2);
+                    long finish2 = System.currentTimeMillis();
+                    long timeConsumedMillis2 = finish2 - start2;
+                    System.out.println("Время выполнения: " + timeConsumedMillis2);
                     //неообходим собрать результаты работы всех тредов и найти по ним среднее арифметическое
                     break;
+
+                case "5":
+
+                    List<String> fileNames2 = MyFileUtils.getFilesNames();
+                    if (fileNames2.size() > 10) {
+                        countThreads = 10;
+                    } else {
+                        countThreads = fileNames2.size();
+                    }
+                    for (int i = 0; i < countThreads; i++) {
+                        AverageSearchCallable averageSearchCallable = new AverageSearchCallable(fileNames2, i, countThreads);
+                        FutureTask futureTask = new FutureTask(averageSearchCallable);
+//                        futureTask.run();
+                    }
             }
             scan.reset();
         }
